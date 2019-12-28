36d70570bc82c293d6f412a73f10917b327a8b96
khalidmeister <alghani_irfan@yahoo.com>
2019-12-28 21:13:03 +0700

    Adhan Sleeper v0.1

---
+++ 1-Adhan-Sleeper/app.py
@@ -0,0 +1,85 @@
+import os
+import requests
+import datetime
+import time
+from dateutil.parser import parse
+
+class Waktu:
+	def __init__(self, waktu):
+		self.waktu = waktu
+		self.jam = int(waktu.hour)
+		self.menit = int(waktu.minute)
+		self.detik = int(waktu.second)
+
+	def get_time(self):
+		if self.jam < 10:
+			self.jam = "0" + str(self.jam)
+		else:
+			self.jam = str(self.jam)
+
+		if self.menit < 10:
+			self.menit = "0" + str(self.menit)
+		else:
+			self.menit = str(self.menit)
+
+		return self.jam + self.menit
+
+def new_time(i):
+	jam = int(i[0:2])
+	menit = int(i[2:])
+	menit -= 5
+	if menit < 0:
+		jam -= 1
+		menit = 60 + menit
+	
+	if jam < 10:
+		jam = "0"+str(jam)
+	else:
+		jam = str(jam)
+	
+	if menit < 10:
+		menit = "0"+str(menit)
+	else:
+		menit = str(menit)
+
+	return jam + menit
+
+# Requests data from API (Al-Adhan)
+kota = input("Masukkan nama kota mu: ")
+address = 'http://api.aladhan.com/v1/calendarByCity?city=' + kota + '&country=Indonesia&method=11'
+json_data = requests.get(address).json()
+adhan_time = json_data['data'][0]['timings']
+names = adhan_time
+
+# Buat Jadwal Adzan
+# Delete beberapa waktu
+del names['Sunrise']
+del names['Sunset']
+del names['Imsak']
+del names['Midnight']
+
+# Cetak Waktu Adzan
+waktu = []
+for k, v in names.items():
+	print(k + "		" + v[0:5]) # Ngambil Waktu
+	temp = parse(v[0:5])
+	waktu.append(Waktu(temp))
+
+# Mengambil waktu adzan berikutnya
+now = Waktu(datetime.datetime.now().time()).get_time()
+for i in waktu:
+	i = i.get_time()
+	check = i < now
+	if check:
+		flag = new_time(i)
+		break
+
+# Real-Time
+# Ketika 5 menit sebelum adzan masuk, laptop hibernate
+while True:
+	print(datetime.datetime.now().time())
+	if now == flag:
+		now = Waktu(datetime.datetime.now().time()).get_time()
+		os.system("rundll32.exe powrprof.dll, SetSuspendState 0,1,0")
+		quit()
+	time.sleep(1)
---
+++ 1-Adhan-Sleeper/resource.md
@@ -0,0 +1,18 @@
+# Adhan Sleeper
+Program ini akan menampilkan **jadwal adzan** sesuai dengan kota yang diinginkan dan program ini akan membuat komputer anda **otomatis shutdown** ketika 5 menit sebelum adzan.
+
+Berikut source code nya dan semoga bermanfaat!
+
+Catatan:
+Untuk pemakaiannya, tinggal klik aplikasi tersebut dan biarkan. Pastikan python sudah terinstall dan terkonseksi dengan internet. 
+
+Terima Kasih.
+
+API yang digunakan: [Al Adhan](https://aladhan.com/prayer-times-api)
+
+Sumber dari semua kepusingan:
+- https://docs.python.org/3/library/datetime.html#datetime.time
+- https://stackabuse.com/converting-strings-to-datetime-in-python/
+- https://codebeautify.org/jsonviewer
+- https://stackoverflow.com/questions/510348/how-can-i-make-a-time-delay-in-python
+- https://stackoverflow.com/questions/1831410/python-time-comparison/9716793
